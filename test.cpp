7caa9c9e0f027220768e508d95ffad952e7ce550
cuom1999 <lephuocdinh99@gmail.com>
2019-02-02 10:56:34 -0600

    ABC

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define ld long double
+#define sf scanf
+#define pf printf
+#define pb push_back
+#define mp make_pair
+#define PI ( acos(-1.0) )
+#define IN freopen("input.txt","r",stdin)
+#define OUT freopen("output.txt","w",stdout)
+#define FOR(i,a,b) for(int i=a ; i<=b ; i++)
+#define FORD(i,a,b) for(int i=a ; i>=b ; i--)
+#define INF 1000000000
+#define ll long long int
+#define eps (1e-8)
+#define sq(x) ( (x)*(x) )
+#define all(x) x.begin(),x.end()
+#define flog2(n) 64 - __builtin_clzll(n) - 1
+#define popcnt(n) __builtin_popcountll(n)
+
+using namespace std;
+
+typedef pair < int, int > pii;
+typedef pair < ll, ll > pll;
+
+
+
+int main()
+{IN;
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+	
+    return 0;
+}
